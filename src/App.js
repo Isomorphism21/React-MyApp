f1ea6d584ed00e1874faf0dddc30037036d6df0e
isomorphism21 <isomorphismay1@gmail.com>
2023-09-18 22:48:14 -0500

    feat: :sparkles: Funcionalidad Crud pedida en clase completada

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import './App.css';
+import { useState } from 'react';
 
-const array = ["Prueba"];
-function ingresar(){
-    array.push(document.getElementById("input").value);
-    console.log(document.querySelector("input").value);
-    console.log(array);
-}
+let idDatos = 0;
 
 function App() {
+  const [dato, setDato] = useState('');
+  const [array, setArray] = useState([]);
+  const [editarID, seteditarID] = useState(null);
+  const [editarValorInput, seteditarValorInput] = useState('');
+
+  const agregarDato = () => {
+    setArray([...array, { id: idDatos++, dato: dato }]);
+    setDato('');
+  };
+
+  const eliminarDato = (id) => {
+    const nuevoArray = array.filter(element => element.id !== id);
+    setArray(nuevoArray);
+  };
+
+  const editarDato = (id) => {
+    seteditarID(id);
+    const element = array.find(element => element.id === id);
+    seteditarValorInput(element.dato);
+  };
+
+  const guardarEdicion = (id) => {
+    const nuevoArray = array.map(element => {
+      if (element.id === id) {
+        return { ...element, dato: editarValorInput };
+      }
+      return element;
+    });
+    setArray(nuevoArray);
+    seteditarID(null);
+  };
+
   return (
-    <div>
+    <main>
+      <h1>Actividad Crud React</h1>
       <div>
-        <input id="input" placeholder="Ingresar" type="text"/>
-        <button onClick={ingresar}>Ingresar</button>
+        <input
+          placeholder='Ingresar Dato'
+          value={dato}
+          onChange={e => setDato(e.target.value)}
+        />
+        <button onClick={agregarDato}>Agregar</button>
       </div>
       <div>
-          {
-            array.map((element) =>
-              <p key={element}>{element}</p>
-            )
-          }
+        {array.map(element => (
+          <div className='puestoDatos' key={element.id}>
+            {editarID === element.id ? (
+              <>
+                <input
+                  value={editarValorInput}
+                  onChange={e => seteditarValorInput(e.target.value)}
+                />
+                <button onClick={() => guardarEdicion(element.id)}>Guardar</button>
+              </>
+            ) : (
+              <>
+                <p>{element.dato}</p>
+                <button onClick={() => editarDato(element.id)}>Editar</button>
+                <button onClick={() => eliminarDato(element.id)}>Eliminar</button>
+              </>
+            )}
+          </div>
+        ))}
       </div>
-    </div>
+    </main>
   );
 }
 
